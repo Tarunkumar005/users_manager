03729469d1c03eb887eac9854ff3ab5b49db3b03
Tarun <tarunsince05@gmail.com>
2025-05-01 15:26:55 +0530

    changes done

--- _server/index.js
+++ _server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const cors = require('cors'); // 🔥 ADD THIS
 
+require('dotenv').config();
+
 const app = express();
 const port = 3002;
 
@@ -11,15 +13,23 @@ app.use(cors());
 // Middleware
 app.use(bodyParser.json());
 
+// const conn = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '1123',
+//   port: '3306',
+//   database: 'xyz',
+// });
+
 // MySQL config
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1123',
-    port: '3306',
-    database: 'xyz',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
 });
-
 // Register Route
 app.post('/register', (req, res) => {
     const { username, email, password } = req.body;
