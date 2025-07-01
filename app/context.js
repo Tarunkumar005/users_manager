e1e3040aca8b4b135bdc64dc2dbdf421c652b5f8
Tarun <tarunsince05@gmail.com>
2025-07-01 13:23:13 +0530

    Update context.js(corrected host in context)

--- app/context.js
+++ app/context.js
@@ -7,9 +7,9 @@ const AuthContext = createContext();
 
 // const host = "http://localhost:3002"; // ✅ local dev backend
 
-const host = "https://backend-production-b164.up.railway.app/";
+const host = "https://backend-production-b164.up.railway.app";
 
-
+s
 export const AuthProvider = ({ children }) => {
   const router = useRouter();
 
